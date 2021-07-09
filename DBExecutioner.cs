4f5ff38243a74d0e206c8eed5c5e5d6f15f7835f
Kizeldur <75386872+Kizeldur@users.noreply.github.com>
2021-07-09 15:22:58 +0300

    Add files via upload

--- DBExecutioner.cs
+++ DBExecutioner.cs
@@ -102,6 +102,37 @@ namespace BDExecution
             reader.Close();
             return roles;
         }
+		
+		public Account  GiveMeSomeShitAssAccount(int Id){
+			var commandString = $"SELECT * table_account WHERE ID = {Id.ToString()}";
+            var reader = Execute(commandString);
+            
+            var account = new Account
+            {
+                Id = reader.GetInt32("Id"),
+                Login = reader.GetString("Login"),
+                Password = reader.GetString("Password"),
+                IsActive = reader.GetBoolean("IsActive")
+            };
+                
+            reader.Close();
+            return account;
+		}
+		
+		public bool CheckTheAccountUpdate( SomeTypeToBeWritten bind){
+			var account = GiveMeSomeShitAssAccount(bind.accountId);
+			
+			if(!account){
+				throw new Exception();
+			}
+			if(bind.accountId == account.Id && bind.login == account.Login && bind.password == account.Password && bind.isActive == account.IsActive){
+				reader.Close();
+				return true;
+			}	
+                
+            reader.Close();
+            return false;
+		}
 
         public MySqlDataReader Execute(string commandString)
         {
@@ -110,7 +141,7 @@ namespace BDExecution
             return reader;
         }
 
-        public IEnumerable<(string, string, bool, string)> GiveMeSomeShitAssAccountRoleList()
+        public IEnumerable<(int accountid, int roleId, string login,  string password, bool isActive, string roleName)> GiveMeSomeShitAssAccountRoleList()
         {
             var accounts = GiveMeAccountList();
             var roles = GiveMeRoleList();
@@ -119,40 +150,39 @@ namespace BDExecution
             var bind = from acc_role in accs_roles
                 join account in accounts on acc_role.AccountId equals account.Id
                 join role in roles on acc_role.RoleId equals role.Id
-                select (account.Login, account.Password, account.IsActive, role.RoleName);
+                select (account.Id, role.Id, account.Login,  account.Password, account.IsActive, role.RoleName);
             return bind;
         }
 
-        public void UpdateDB()
+        public void UpdateDB(IEnumerable<(int accountid, int roleId, string login,  string password, bool isActive, string roleName)> bind)
         {
-            var commandString = "Update adawdawd";
-            
-            MySqlCommand command = new MySqlCommand(commandString, db);
-            var reader = command.ExecuteReader();
-            reader.Close();
-            commandString = "Insert adawaad";
-            command = new MySqlCommand(commandString, db);
-            reader = command.ExecuteReader();
-            reader.Close();
+			var commandString = "Update adawdawd";
+			
+			 foreach(var obj in bind){
+				 try{
+					if(CheckTheAccountUpdateUpdate(obj)){
+						command = $"UPDATE table_account SET Login == {obj.login}, Password = {obj.password}, IsActive = {obj.isActive} Where Id = {obj.isActive}";
+						MySqlCommand command = new MySqlCommand(commandString, db);
+						command.ExecuteNonQuery();
+					}
+				 }
+				 catch(new Exception()){
+					commandString = "INSERT INTO table_account SET column = 1, id=1 ON DUPLICATE KEY UPDATE column = column + 1";
+					command = new MySqlCommand(commandString, db);
+					
+				 }
+				 command.ExecuteNonQuery();
+			 }	
         }
         
-         public IEnumerable<(string, string, bool, string)> GiveGiveMeSomeShitAssAccountRoleList(List<Account> accounts, List<Role> roles, List<Account_Role>accs_roles )
+         public IEnumerable<(string, int, string, bool, string, int)> GiveGiveMeSomeShitAssAccountRoleList(List<Account> accounts, List<Role> roles, List<Account_Role>accs_roles )
         {
-            //var accounts = GiveMeAccountList();
-            //var roles = GiveMeRoleList();
-            //var accs_roles = GiveMeAccountRoleList();
-
-            /*var bind = from acc_role in accs_roles
-                from account in accounts
-                join role in roles on
-                where account.Id == acc_role.AccountId
-                where role.Id == acc_role.RoleId
-                select (account.Login, account.Password, account.IsActive, role.RoleName);*/
             
-            var bind2 = from acc_role in accs_roles
+            
+            IEnumerable<(int accountid, int roleId, string login,  string password, bool isActive, string roleName) bind2 = from acc_role in accs_roles
                 join account in accounts on acc_role.AccountId equals account.Id
                 join role in roles on acc_role.RoleId equals role.Id
-                select (account.Login, account.Password, account.IsActive, role.RoleName);
+                select (account.Id, role.Id, account.Login,  account.Password, account.IsActive, role.RoleName);
 
             return bind2;
         }
